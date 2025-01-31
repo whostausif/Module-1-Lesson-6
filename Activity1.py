59e21c28ce84204608eb81886cb947cd1ec80e92
whostausif <tausifwali@gmail.com>
2025-01-31 19:51:23 +0600

    Lesson 6 Activity 1

---
+++ Activity1.py
@@ -0,0 +1,20 @@
+a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print("All the numbers have boolean value as True")
+
+else:
+    print("Atleast one number has boolean value as False")
+
+
+a = 10
+b = -10
+c = 0
+
+if a>0 or b>0:
+    print("Either any value greater than 0")
+
+else:
+    print("No value is greater than 0")
