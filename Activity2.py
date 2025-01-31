ebc09bc4fc620242744ab881fbbc4161cc46b936
whostausif <tausifwali@gmail.com>
2025-01-31 19:57:40 +0600

    Lesson 6 Activity 2

---
+++ Activity2.py
@@ -0,0 +1,14 @@
+a = 10
+b = 12
+c = 12
+
+print(a != b)
+
+print(b != c)
+
+x = int(input("Enter a number: "))
+if x%2 != 0:
+    print(x,"is an odd number")
+
+else:
+    print(x,"is an even number")
